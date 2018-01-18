860ff9599fd41905e64df4763c41605fc1b79b18
savagemechanic <seighaf@yahoo.com>
2018-01-18 18:07:40 +0100

    changed to server api

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
-export const API = 'http://localhost:5000/'
-// export const API = 'http://46.101.83.13:5000/'
+export const API = 'http://139.162.247.100:5000/'
+// export const API = 'http://localhost:5000/'
 
 export var HOST = 'http://127.0.0.1:8080/#/'
 export var blackAxios = axios.create({
