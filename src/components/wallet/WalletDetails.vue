56b8caca366569c9a60fa84ef2072eceba309c47
tay.andrew <tay.andrew@yahoo.com>
2018-01-31 22:43:14 +0100

    main details

--- src/components/wallet/WalletDetails.vue
+++ src/components/wallet/WalletDetails.vue
@@ -1,24 +1,10 @@
 <template>
 <span>
-    <div class="col-md-12" v-for="(transaction, index) in transactions" :key="index">
-        <SavageWell :title="transaction.month" :defaultState="true">
-            <div slot="content" class="row">
-                <div class="col-md-12" v-for="(trans, index) in transaction.transactions" :key="index">
-                    <Transaction :description="trans.description"
-                                 :amount="trans.amount"
-                                 :currency="trans.currency"
-                                 :type="trans.type"
-                                 :date="trans.date">
-
-                    </Transaction>
-                </div>
-            </div>
-        </SavageWell>
-    </div>
+    <transaction :show="details"></transaction>
 </span>
 </template>
 <script>
-  import Transaction from '../transactions/transaction'
+  import Transaction from './details/transactions.vue'
 
   export default {
     components: {
@@ -26,53 +12,7 @@
     },
     data () {
       return {
-        transactions: [
-          {
-            month: 'December 2017',
-            transactions: [
-              {
-                type: 'subtract',
-                description: 'Bought BTC 0.00900 for NGN 20,000.00',
-                amount: '-20,000.00',
-                currency: 'NGN',
-                date: '9th December 2017',
-              },
-              {
-                type: 'add',
-                description: 'Bought BTC 0.00900 for NGN 20,000.00',
-                amount: '+0.00900',
-                currency: 'BTC',
-                date: '9th December 2017',
-              }
-            ]
-          },
-          {
-            month: 'January 2018',
-            transactions: [
-              {
-                type: 'subtract',
-                description: 'Transaction Fee',
-                amount: '-0.0001',
-                currency: 'BTC',
-                date: '1st January 2018',
-              },
-              {
-                type: 'add',
-                description: 'Sold BTC 0.0172090 for NGN 200,000.00',
-                amount: '+200,000.00',
-                currency: 'NGN',
-                date: '1st January 2018',
-              },
-              {
-                type: 'subtract',
-                description: 'Sold BTC 0.0172090 for NGN 200,000.00',
-                amount: '-0.0172090',
-                currency: 'BTC',
-                date: '1st January 2018',
-              },
-            ]
-          },
-        ]
+        details:true
       }
     },
     methods: {
